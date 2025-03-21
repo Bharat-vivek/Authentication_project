51fbd5e76d40edced6c78c135926d5f02f2d48a2
Bharat Verma <118604804+Bharat-vivek@users.noreply.github.com>
2025-03-21 21:54:14 +0530

    .

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./.env" });
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:5173"],
     methods: ["POST", "GET"],
     credentials: true,
 }));
