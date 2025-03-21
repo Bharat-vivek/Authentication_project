1fb28c8fe46f8af6827a65534ac840c085dbf570
Bharat Verma <118604804+Bharat-vivek@users.noreply.github.com>
2025-03-21 21:42:49 +0530

    .

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/register" element={<Signup />} />
         <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
